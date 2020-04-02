44e3a23bb5b16bed0ea2cb781a9c2fe2b23df57c
Rob Mathews <robbis@gmail.com>
2020-04-02 11:48:41 +0100

    data pushing through

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <template>
 	<div>
 		<h1>UK Energy sources</h1>
+		<energy-graph :energy_data="energy_mix"></energy-graph>
 	</div>
 </template>
 
 <script>
+import EnergyGraph from './components/EnergyGraph';
+
 export default {
 	data() {
 		return {
-			energy_mix: {}
+			energy_mix: {},
+			graph_mix: []
 		};
 	},
 	mounted() {
 		fetch('https://api.carbonintensity.org.uk/generation')
 			.then(res => res.json())
-			.then(json => (this.energy_mix = json))
-			.then(console.log(this.energy_mix));
+			.then(json => (this.energy_mix = json.data.generationmix))
+			.then(console.log('starting loop'))
+			.then(convertData())
+			.then(console.log('finishing loop'));
+	},
+	components: {
+		'energy-graph': EnergyGraph
+	},
+	methods: {
+		convertData: function() {
+			this.energy_mix.forEach(d => console.log(d));
+		}
 	}
 };
 </script>
---
+++ src/components/EnergyGraph.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<p v-for="mix in energy_data" v-bind:key="mix">
+			{{ mix.fuel }} - {{ mix.perc }}
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'energy-graph',
+	props: ['energy_data']
+};
+</script>
+
+<style scoped></style>
