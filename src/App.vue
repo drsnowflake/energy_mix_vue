753947c11d8d030fec9db29276d1ee64150bd1dd
Rob Mathews <robbis@gmail.com>
2020-04-02 12:29:15 +0100

    pre lunch

--- src/App.vue
+++ src/App.vue
@@ -11,25 +11,30 @@ import EnergyGraph from './components/EnergyGraph';
 export default {
 	data() {
 		return {
-			energy_mix: {},
+			energy_mix: [],
 			graph_mix: []
 		};
 	},
+	// computed: {
+	// 	convertData() {
+	// 		console.log(this.energy_mix);
+	// 	}
+	// },
 	mounted() {
 		fetch('https://api.carbonintensity.org.uk/generation')
 			.then(res => res.json())
-			.then(json => (this.energy_mix = json.data.generationmix))
-			.then(console.log('starting loop'))
-			.then(convertData())
-			.then(console.log('finishing loop'));
+			// .then(json =>
+			// 	json.data.generationmix.forEach(d => this.energy_mix.push(d))
+			.then(data => this.convertData(data));
+	},
+	methods: {
+		convertData: function(data) {
+			console.log(data.data.generationmix);
+			data.forEach(data => this.energy_mix.push(data.data.generationmix.fuel));
+		}
 	},
 	components: {
 		'energy-graph': EnergyGraph
-	},
-	methods: {
-		convertData: function() {
-			this.energy_mix.forEach(d => console.log(d));
-		}
 	}
 };
 </script>
